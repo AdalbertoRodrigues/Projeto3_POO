30503ed056d5f200a5103509c274f1ad708e4ea2
Diogo Pacheco Pereira <Bruno@Bruno-PC>
2017-09-26 12:15:01 -0300

    Adicionado a função getClientes() no Banco de Dados

--- src/java/br/com/fatec/oo/Bd.java
+++ src/java/br/com/fatec/oo/Bd.java
@@ -6,10 +6,17 @@ public class Bd {
     
     //Criando lista de contatos
     private static ArrayList<Fornecedor> fornecedores;
+    private static ArrayList<Cliente> clientes;
     public static ArrayList<Fornecedor> getFornecedores () {
         if (fornecedores==null) {
             fornecedores = new ArrayList<>();
         }
         return fornecedores;
+    }
+    public static ArrayList<Cliente> getClientes () {
+        if (clientes==null) {
+            clientes = new ArrayList<>();
+        }
+        return clientes;
     }
 }
